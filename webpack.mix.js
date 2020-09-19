fb494151a616a35d4e8e4f918a3d1aa63bc20a21
DedeSnrwn13 <sunarwandede13@gmail.com>
2020-09-20 03:31:34 +0700

    add education page and gallery page, done

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,7 +8,9 @@ mix.copy('resources/js/*.js', 'dist/assets/js/');
 
 mix.sass('resources/sass/style.scss', 'dist/assets/css')
    .sass('resources/sass/search.scss', 'dist/assets/css')
-
+   .sass('resources/sass/edukatif.scss', 'dist/assets/css')
+   .sass('resources/sass/navbar.scss', 'dist/assets/css')
+   .sass('resources/sass/galeri.scss', 'dist/assets/css')
 
 
 .options({
